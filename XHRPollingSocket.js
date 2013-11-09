a0ff8ea3424721301b43b30abf029ea325c858b7
Ian Hansen <ian@supershabam.com>
2013-11-09 10:35:14 -0700

    send close event; clear out closed socket

--- XHRPollingSocket.js
+++ XHRPollingSocket.js
@@ -1,27 +1,36 @@
-var uuid = require('uuid')
+var events = require('events')
+  , util   = require('util')
+  , uuid   = require('uuid')
 
 module.exports = XHRPollingSocket
 
 // ws should be open and valid
 function XHRPollingSocket(ws) {
+  events.EventEmitter.call(this)
   var self       = this
   this.buffer    = []
   this.is_closed = false
   this.id        = uuid.v1()
   this.ws        = ws
   this.ws.once('close', function() {
+    console.log('websocket:close')
     self.close()
   })
   this.ws.on('message', function(data) {
     self.send_client(data)
   })
 }
+util.inherits(XHRPollingSocket, events.EventEmitter)
 
 XHRPollingSocket.prototype.close = function() {
   if (!this.is_closed) {
-    this.send_client({event: 'close'})
+    this.buffer.push({event: 'close'})
+    if (this.client) {
+      this.set_client(this.client)
+    }
     this.ws.removeAllListeners()
     this.ws.close()
+    this.emit('close')
   }
   this.is_closed = true
 }
