855e733f1e0d8fef77648899a34442e312b33471
Ian Hansen <ian@supershabam.com>
2013-11-08 21:16:34 -0700

    recurse!

--- public/turst.js
+++ public/turst.js
@@ -1,11 +1,18 @@
 $.post('/polysocket/create', {target_ws: "ws://echo.websocket.org"}).done(function(response) {
   var socket_id = response.socket_id
-  $.post('/polysocket/socket', {socket_id: socket_id, data: "Oh hai?"}).done(function() {
-    console.log('sent dataz')
-  })
-  $.get('/polysocket/xhr-poll', {socket_id: socket_id}).done(function(response) {
-    console.log('xhr poll res', response)
-  })
+  forever(socket_id)
+  setInterval(function() {
+    $.post('/polysocket/socket', {socket_id: socket_id, data: "Oh hai?"}).done(function() {
+    }).fail(function(err){
+      console.error(err)
+    })
+  }, 3000)
 }).fail(function(err) {
   console.error(err)
 })
+
+function forever(socket_id) {
+  $.get('/polysocket/xhr-poll', {socket_id: socket_id}).done(function(response) {
+    console.log('data', response.data)
+  }).complete(forever.bind(null, socket_id))
+}
